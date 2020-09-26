b7fcb26add7e9b846b8c7711f513ff5f8db6e480
Matthew J. Hughes <mjhughes707@gmail.com>
2020-09-26 16:25:29 -0700

    moved Button to components folder + styling update

--- src/assets/styles.js
+++ src/assets/styles.js
@@ -3,5 +3,7 @@ import { makeStyles } from "@material-ui/core/styles";
 export const useStyles = makeStyles({
   button: {
     borderRadius: "20px",
+    width: "auto",
+    minWidth: "12rem",
   },
 });
