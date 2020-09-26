7734235ebaf8e5915e7deedb58bfedb874bafb56
Matthew J. Hughes <mjhughes707@gmail.com>
2020-09-26 11:51:27 -0700

    updated styling + added Google Fonts

--- src/App.js
+++ src/App.js
@@ -3,21 +3,24 @@ import { Switch, Route, Redirect } from "react-router-dom";
 import Home from "./components/Home";
 import Pokemon from "./components/Pokemon";
 import Catch from "./components/Catch";
+import "./App.css";
 
 function App() {
   return (
-    <Switch>
-      <Route exact path="/">
-        <Home />
-      </Route>
-      <Route exact path="/pokemon/:id">
-        <Pokemon />
-      </Route>
-      <Route exact path="/catch">
-        <Catch />
-      </Route>
-      <Redirect to="/" />
-    </Switch>
+    <main>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/pokemon/:id">
+          <Pokemon />
+        </Route>
+        <Route exact path="/catch">
+          <Catch />
+        </Route>
+        <Redirect to="/" />
+      </Switch>
+    </main>
   );
 }
 
--- src/assets/styles.js
+++ src/assets/styles.js
@@ -1,13 +1,6 @@
 import { makeStyles } from "@material-ui/core/styles";
 
 export const useStyles = makeStyles({
-  colors: {
-    violet: "#583F8C",
-    magenta: "#BF0449",
-    skyblue: "#05C7F2",
-    teal: "#03A6A6",
-    gold: "#F2D98D",
-  },
   button: {
     borderRadius: "20px",
   },
