070cd2d86536c8c652c28f5144f1f103645d323e
Matthew J. Hughes <mjhughes707@gmail.com>
2020-09-26 17:10:07 -0700

    updated Header to link back to Home page

--- src/parts/Header.js
+++ src/parts/Header.js
@@ -1,5 +1,10 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-const Header = () => <header className="header">Pokatchér</header>;
+const Header = () => (
+  <header className="header">
+    <Link to="/">Pokatchér</Link>
+  </header>
+);
 
 export default Header;
