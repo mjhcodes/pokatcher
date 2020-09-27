50e1702bad4ba233bca40a8f79adece0d526c716
Matthew J. Hughes <mjhughes707@gmail.com>
2020-09-27 16:36:19 -0700

    created modal for handling submission w/ custom name

--- src/assets/styles.js
+++ src/assets/styles.js
@@ -1,9 +1,33 @@
 import { makeStyles } from "@material-ui/core/styles";
 
-export const useStyles = makeStyles({
+export const useStyles = makeStyles((theme) => ({
   button: {
     borderRadius: "20px",
     width: "auto",
     minWidth: "12rem",
   },
-});
+  modal: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  paper: {
+    backgroundColor: theme.palette.background.paper,
+    border: "5px solid #05c7f2",
+    borderRadius: "20px",
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(7, 5, 9),
+    textAlign: "center",
+  },
+  form: {
+    "& > *": {
+      marginTop: "3rem",
+      textAlign: "center",
+    },
+  },
+  input: {
+    "& > *": {
+      textAlign: "center",
+    },
+  },
+}));
---
+++ src/components/NameModal.js
@@ -0,0 +1,82 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import { FormControl, Input, Modal, Backdrop, Fade } from "@material-ui/core";
+import { useStyles } from "../assets/styles";
+import { isObjEmpty } from "../utils/util";
+import Button from "./Button";
+
+const NameModal = ({
+  isModalOpen,
+  handleModalClose,
+  my_collection,
+  caughtPokemon,
+}) => {
+  const [name, setName] = useState("");
+  const { modal, paper, form, input } = useStyles();
+
+  const types =
+    !isObjEmpty(caughtPokemon) &&
+    caughtPokemon.types.map((type) => type.type.name);
+
+  const confirmCatch = () => {
+    const obj = { ...caughtPokemon, name };
+    localStorage.setItem("featured_pokemon", JSON.stringify(obj));
+    localStorage.setItem(
+      "my_collection",
+      JSON.stringify([...my_collection, obj])
+    );
+  };
+
+  return (
+    <Modal
+      aria-labelledby="modal-title"
+      aria-describedby="modal-subtitle"
+      className={modal}
+      open={isModalOpen}
+      onClose={handleModalClose}
+      closeAfterTransition
+      BackdropComponent={Backdrop}
+      BackdropProps={{
+        timeout: 500,
+      }}
+    >
+      <Fade in={isModalOpen}>
+        <div className={paper}>
+          <h2 id="modal-title" className="modal__title">
+            AWESOME!
+          </h2>
+          <p id="modal-subtitle" className="modal__subtitle">
+            {!isObjEmpty(caughtPokemon) && `You caught ${types.join(" / ")}!`}
+          </p>
+          <FormControl className={form} noValidate autoComplete="off">
+            <label htmlFor="new-name" className="modal__label">
+              Now let's give it a name!
+            </label>
+            <Input
+              required
+              id="new-name"
+              className={input}
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+            <Button
+              to={`/pokemon/${caughtPokemon.id}`}
+              label="SUBMIT"
+              disabled={!name}
+              onClick={() => confirmCatch()}
+            />
+          </FormControl>
+        </div>
+      </Fade>
+    </Modal>
+  );
+};
+
+NameModal.propTypes = {
+  isModalOpen: PropTypes.bool.isRequired,
+  handleModalClose: PropTypes.func.isRequired,
+  my_collection: PropTypes.array.isRequired,
+  caughtPokemon: PropTypes.object.isRequired,
+};
+
+export default NameModal;
