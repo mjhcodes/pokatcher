38fbc1e4b6fe05eb6544ae46c86bea90cfead49e
Matthew J. Hughes <mjhughes707@gmail.com>
2020-09-27 16:34:49 -0700

    updated Thumbnail component to handle json data

--- src/components/Thumbnail.js
+++ src/components/Thumbnail.js
@@ -1,14 +1,26 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const Thumbnail = ({ url, color }) => (
-  <button className="catch__thumbnail" style={{ border: `4px solid ${color}` }}>
-    <img src={url} alt="Random Pokemon character" />
-  </button>
-);
+const Thumbnail = ({ selected, pokemon, setCaughtPokemon, url, color }) => {
+  return (
+    <button
+      onClick={() => setCaughtPokemon(pokemon)}
+      className={
+        selected
+          ? "catch__thumbnail catch__thumbnail-active"
+          : "catch__thumbnail"
+      }
+      style={{ border: `4px solid ${color}` }}
+    >
+      <img src={pokemon.sprites.front_default} alt="Random Pokemon character" />
+    </button>
+  );
+};
 
 Thumbnail.propTypes = {
-  url: PropTypes.string.isRequired,
+  selected: PropTypes.bool.isRequired,
+  pokemon: PropTypes.object.isRequired,
+  setCaughtPokemon: PropTypes.func.isRequired,
   color: PropTypes.string.isRequired,
 };
 
