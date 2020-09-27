22ab9865a161f5bdb0121f4881588a8d5debd4df
Matthew J. Hughes <mjhughes707@gmail.com>
2020-09-26 20:12:29 -0700

    updated Home page with collection / card

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,21 @@
 import React, { useEffect } from "react";
 import { setRandomOffset } from "../services/fetchData";
+import { placeholder } from "../assets/placeholder";
 import Button from "../components/Button";
+import Card from "../components/Card";
 
 const Home = () => {
   useEffect(() => {
     setRandomOffset();
   }, []);
+
+  const isCollection = localStorage.getItem("collection");
+
+  const collection = isCollection
+    ? localStorage.getItem("collection")
+    : [placeholder];
+
+  const collectionLength = isCollection ? collection.length : 0;
 
   return (
     <article>
@@ -14,15 +24,24 @@ const Home = () => {
           Pokatchér is the placé to katch Pokémon!
         </h1>
       </header>
-      <section>
-        <Button to="/pokemon/1" label="SELECT POKÉMON" />
+
+      <section className="column centered">
+        <h2 className="home__collection-title">MY COLLECTION</h2>
+        {collection.map((pokemon, idx) => (
+          <Card key={`pokemon_card_${idx}`} pokemon={pokemon} />
+        ))}
+        <p className="home__collection-count">
+          There are {collectionLength} Pokemon in your collection
+        </p>
       </section>
-      <section>
+
+      <section className="home__instructions-section">
         <p className="home__instructions">
           To view a Pokemon from your collection in more detail, click its image
           above, or, click the button below to catch some more!
         </p>
       </section>
+
       <section>
         <Button to="/catch" label="CATCH POKÉMON" />
       </section>
