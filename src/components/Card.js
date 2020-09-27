1dc1d4a47aa2e26db2d5d2a1bf74823aff320586
Matthew J. Hughes <mjhughes707@gmail.com>
2020-09-27 16:35:09 -0700

    updated Card component to handle json data

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,16 +3,23 @@ import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 
 const Card = ({ pokemon }) => {
-  const { id, name, img } = pokemon;
+  const {
+    id,
+    name,
+    sprites: { front_default },
+  } = pokemon;
   return (
     <div className={id ? "card" : "card card-disabled"}>
       <Link
         to={id ? `/pokemon/${id}` : "#"}
         onClick={() =>
-          localStorage.setItem("selected", JSON.stringify(pokemon))
+          localStorage.setItem("featured_pokemon", JSON.stringify(pokemon))
         }
       >
-        <img src={img} alt="Pokemon character from user's collection" />
+        <img
+          src={front_default}
+          alt="Pokemon character from user's collection"
+        />
         <div className="card__label">{name}</div>
       </Link>
     </div>
