7ad96651acd955e4e0d18aae4e7e602d7731c93d
Matthew J. Hughes <mjhughes707@gmail.com>
2020-09-27 19:30:31 -0700

    installed m-ui lab + set up collection pagination

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,21 +1,38 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import { Pagination } from "@material-ui/lab";
 import { setRandomIdArray } from "../services/fetchData";
 import { placeholder } from "../assets/placeholder";
 import Button from "../components/Button";
 import Card from "../components/Card";
 
 const Home = () => {
+  const [activePokemon, setActivePokemon] = useState(placeholder);
+
+  const loadedCollection = localStorage.getItem("my_collection");
+  const featuredPokemon = localStorage.getItem("featured_pokemon");
+
+  const myCollection = loadedCollection
+    ? JSON.parse(loadedCollection)
+    : [placeholder];
+  const pendingPokemon = featuredPokemon
+    ? JSON.parse(featuredPokemon)
+    : myCollection[0];
+
+  const collectionLength = loadedCollection ? myCollection.length : 0;
+
+  const defaultPage =
+    loadedCollection && featuredPokemon
+      ? myCollection.findIndex((poke) => poke.id === pendingPokemon.id) + 1
+      : 1;
+
   useEffect(() => {
     setRandomIdArray();
+    setActivePokemon(pendingPokemon);
   }, []);
 
-  const isCollectionLoaded = localStorage.getItem("my_collection");
-
-  const my_collection = isCollectionLoaded
-    ? JSON.parse(localStorage.getItem("my_collection"))
-    : [placeholder];
-
-  const collectionLength = isCollectionLoaded ? my_collection.length : 0;
+  const handleChange = (e, value) => {
+    setActivePokemon(myCollection[value - 1]);
+  };
 
   return (
     <article>
@@ -27,9 +44,20 @@ const Home = () => {
 
       <section className="column centered">
         <h2 className="home__collection-title">MY COLLECTION</h2>
-        {my_collection.map((pokemon, idx) => (
-          <Card key={`pokemon_card_${idx}`} pokemon={pokemon} />
-        ))}
+        <Card pokemon={activePokemon} />
+        <Pagination
+          count={collectionLength}
+          defaultPage={defaultPage}
+          siblingCount={1}
+          boundaryCount={1}
+          hidePrevButton={collectionLength === 0}
+          hideNextButton={collectionLength === 0}
+          onChange={handleChange}
+          variant="outlined"
+          color="secondary"
+          size="small"
+          style={{ marginTop: "1rem" }}
+        />
         <p className="home__collection-count">
           There are {collectionLength} Pokemon in your collection
         </p>
