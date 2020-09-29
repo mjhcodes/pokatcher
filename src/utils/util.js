5d482c977b60317126ef607dd587ea7b5e4c9fb3
Matthew J. Hughes <mjhughes707@gmail.com>
2020-09-28 17:57:11 -0700

    added new util function

--- src/utils/util.js
+++ src/utils/util.js
@@ -1,3 +1,7 @@
 export const isObjEmpty = (obj) => {
   return Object.keys(obj).length === 0;
 };
+
+export const getRandomNumArray = (max) => {
+  return new Array(10).fill(1).map((n) => n * Math.ceil(Math.random() * max));
+};
