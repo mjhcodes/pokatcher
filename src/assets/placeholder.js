851e90d2bc5500a61e38f3d7da46dd1d3090290c
Matthew J. Hughes <mjhughes707@gmail.com>
2020-09-29 14:32:13 -0700

    post-deployment updates

--- src/assets/placeholder.js
+++ src/assets/placeholder.js
@@ -1,6 +1,8 @@
 export const placeholder = {
   id: null,
-  sprites: { front_default: require("../assets/images/pikachu-outline.jpeg") },
+  sprites: {
+    front_default: `${process.env.PUBLIC_URL}/pikachu-outline.jpeg`,
+  },
   name: "???",
   types: [{ type: { type: { name: "" } } }],
   abilities: [{ ability: { ability: { name: "" } } }],
